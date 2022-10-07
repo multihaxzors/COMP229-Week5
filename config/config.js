df617d8ff69adb49c1c0723ed98f2b9d205a81cf
multihaxzors <43578117+multihaxzors@users.noreply.github.com>
2022-10-07 19:14:30 -0400

    Mongo Atlas Setup Completed

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
 export const Secret = "MySecret";
-export const MongoURI  = "mongodb://127.0.0.1/media"
+export const MongoURI  = "mongodb+srv://multihaxzors:qg355dbh@comp229-week5.0po4mzd.mongodb.net/media?retryWrites=true&w=majority"
